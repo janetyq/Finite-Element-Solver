includes forward simulation of various springs topopt gradient fixed

Solver.py
@@ -168,12 +168,19 @@ class Solver:
     def _solve_linear_elastic(self):
         u = np.zeros(2 * len(self.mesh.points))
         u[self.fixed] = self.boundary_conditions.fixed_values
+
+        if 'rho' in self.equation.parameters:
+            rho = self.equation.parameters['rho']
+        else:
+            rho = np.full(len(self.mesh.faces), 1)
+
+        E_min, p = 1e-9, 3
         
         E = self.equation.parameters['E']
-        E = np.full(len(self.mesh.faces), E)
+        E = E_min + np.full(len(self.mesh.faces), E) * rho**p
         nu = self.equation.parameters['nu']
         nu = np.full(len(self.mesh.faces), nu)
-        
+
         material_func = np.vstack([E, nu]).T
         K = assemble_matrix(self.mesh.points, self.mesh.faces, calculate_element_stiffness_matrix, func=material_func, dim=2)
         K_mod = K[np.ix_(self.free, self.free)]
@@ -183,7 +190,7 @@ class Solver:
         deformed_mesh = self.mesh.get_deformed_mesh(u)
         self.solution.set_values("u", u)
         self.solution.set_values("deformed_mesh", deformed_mesh)
-        self.solution.set_values("none", np.zeros(len(self.mesh.faces)))
+        self.solution.set_values("rho", self.equation.parameters.get('rho', np.full(len(self.mesh.faces), 1)))
 
         self.B = np.array([calculate_B(element, (material_func, e_idx)) for e_idx, element in enumerate(self.mesh.points[self.mesh.faces])]) # gradient matrix
         self.D = np.array([calculate_D(element, (material_func, e_idx)) for e_idx, element in enumerate(self.mesh.points[self.mesh.faces])]) # elasticity matrix
@@ -202,15 +209,19 @@ class Solver:
         self.solution.set_values("strain", np.linalg.norm(eps_faces, axis=-1))
         self.solution.set_values("stress", np.linalg.norm(sigma_faces, axis=-1))
         
-        C_total = 1/2 * u.T @ K @ u
         C_faces = np.zeros(len(self.mesh.faces))
+        force_faces = np.zeros(len(self.mesh.faces))
         for face_idx, face in enumerate(self.mesh.faces):
             e_idxs = np.array([2*face, 2*face+1]).T.flatten()
             u_face = u[e_idxs]
             K_face = calculate_element_stiffness_matrix(self.mesh.points[face], (material_func, face_idx), dim=2)
-            C_faces[face_idx] = 1/2 * u_face.T @ K_face @ u_face
+            C_faces[face_idx] = u_face.T @ K_face @ u_face
+            force_faces[face_idx] = np.linalg.norm(K_face @ u_face)
         self.solution.set_values("compliance", C_faces)
-        self.solution.set_values("total_compliance", C_total)
+        self.solution.set_values("total_compliance", np.sum(C_faces))
+        self.solution.set_values("force_faces", force_faces)
+        force_vertices = self.solution._convert_face_values_to_vertex_values(force_faces)
+        self.solution.set_values("force_vertices", force_vertices)
 
     def adaptive_refinement(self, max_triangles=1000, max_iters=20):
         # TODO: there's a bug somewhere

TopologyOptimizer.py
@@ -3,7 +3,7 @@ from Solver import *
 class TopologyOptimizer:
     def __init__(self, solver, parameters):
         self.solver = solver
-        self.parameters = parameters # iters, volume_frac, alpha, beta
+        self.parameters = parameters # iters, volume_frac, rho=3
         self.solution = Solution(solver.mesh)
         self.face_neighbors = self.solver.mesh.calculate_face_neighbors()
 
@@ -22,7 +22,7 @@ class TopologyOptimizer:
             m = 0.5*(l+r)
             rho_new = rho * np.sqrt(sensitivity / m)
             # rho_new = np.clip(rho_new, rho_new - 0.1, rho_new + 0.1) # change limit
-            rho_new = np.clip(rho_new, 0, 1) 
+            rho_new = np.clip(rho_new, 1e-6, 1) 
 
             if self.solver.mesh.calculate_mean_value(rho_new) < volume_frac:
                 r = m
@@ -30,52 +30,46 @@ class TopologyOptimizer:
                 l = m
         return rho_new
 
-
-    def solve(self):
+    def solve(self, target_compliance=None):
         if self.solver.equation.name == 'linear_elastic':
-            E_0 = self.solver.equation.parameters['E']
             iters, volume_frac = self.parameters['iters'], self.parameters['volume_frac']
-            alpha, beta = self.parameters['alpha'], self.parameters['beta']
             
-            E_min, p = 1e-9, 3
-            rho = np.full(len(self.solver.mesh.faces), volume_frac)
+            E, p = self.solver.equation.parameters['E'], 3
+            rho = self.solver.equation.parameters.get('rho', np.full(len(self.solver.mesh.faces), volume_frac))
             
             rhos, us, deformed_meshes, compliances = [], [], [], []
-            for iter in range(iters+1):
-                self.solver.equation.parameters['E'] = E_min + E_0 * rho**p
+            for iter in range(iters):
+                self.solver.equation.parameters['rho'] = rho # update rho
                 self.solver.solve()
                 C = self.solver.solution.values['total_compliance']
                 C_faces = self.solver.solution.values['compliance']
-                rho_mean = self.solver.mesh.calculate_mean_value(rho)
 
                 u, deformed_mesh = self.solver.solution.values['u'], self.solver.solution.values['deformed_mesh']
-
-                # print results
-                # TODO: print as table
-                print('\nIteration', iter)
-                print('C:', C)
-                print(f'Volume ratio: {rho_mean}')
-                print('max displacement', np.max(u, axis=0))
-
-                scaling = (len(self.solver.mesh.faces)) / C
-
-                # update rho
-                # dC_drho = p * E_0 * rho**(p-1) * C_faces
-                # rho += alpha * scaling * dC_drho + beta * min(volume_frac - rho_mean, 0)
-                sensitivity = self.filter_sensitivity(p * E_0 * rho**(p-1) * C_faces)
-                rho = self.oc_density(rho, sensitivity, volume_frac)
-
-                # # smoothing
-                # smoothed_rho = np.zeros(len(rho))
-                # for face_idx, face in enumerate(self.solver.mesh.faces):
-                #     neighbor_value = np.mean([rho[neighbor_idx] for neighbor_idx in face_neighbors[face_idx]])
-                #     smoothed_rho[face_idx] = 0.5 * neighbor_value + 0.5 * rho[face_idx]
-                # rho = np.clip(smoothed_rho, 0, 1)
 
                 us.append(u.copy())
                 deformed_meshes.append(deformed_mesh)
                 rhos.append(rho.copy())
                 compliances.append(C)
+
+                # print results
+                print('\nIteration', iter)
+                print('C:', C)
+                print('max displacement', np.max(u, axis=0))
+                print('volume fraction:', self.solver.mesh.calculate_mean_value(rho))
+
+                deformed_mesh.plot_colored(rho, save=f"results/rho{iter}.png", cbar_label='Density', title=f'Iteration {iter}, C={C:.4f}')
+
+                # update rho
+                if target_compliance is None:
+                    sensitivity = p/rho * C_faces
+                else:
+                    sensitivity = 2*(C_faces - target_compliance) * p/rho * C_faces
+                sensitivity = self.filter_sensitivity(sensitivity)
+                if volume_frac == 1:
+                    rho += np.clip(0.1 * sensitivity, -0.1, 0.1)
+                    rho = np.clip(rho, 1e-6, 1)
+                else:
+                    rho = self.oc_density(rho, sensitivity, volume_frac)
 
             self.solution.set_values('us', us)
             self.solution.set_values('deformed_meshes', deformed_meshes)
@@ -84,122 +78,3 @@ class TopologyOptimizer:
         else:
             raise ValueError(f'Topology optimization not supported for {self.equation.name} equation')
         pass
-
-    def solve_with_target_compliance(self, target_compliance):
-        he_mesh = HalfEdgeMesh(self.solver.mesh)
-        if self.solver.equation.name == 'linear_elastic':
-            E_0 = self.solver.equation.parameters['E']
-            iters, volume_frac = self.parameters['iters'], self.parameters['volume_frac']
-            alpha, beta = self.parameters['alpha'], self.parameters['beta']
-            
-            E_min, p = 1e-9, 3
-            rho = np.full(len(self.solver.mesh.faces), volume_frac)
-            
-            rhos, us, deformed_meshes, compliances = [], [], [], []
-            for iter in range(iters+1):
-                self.solver.equation.parameters['E'] = E_min + E_0 * rho**p
-                self.solver.solve()
-                C = self.solver.solution.values['total_compliance']
-                C_faces = self.solver.solution.values['compliance']
-                rho_mean = self.solver.mesh.calculate_mean_value(rho)
-
-                u, deformed_mesh = self.solver.solution.values['u'], self.solver.solution.values['deformed_mesh']
-
-                # print results
-                print('\nIteration', iter)
-                print('C:', C)
-                print(f'Volume ratio: {rho_mean}')
-                print('max displacement', np.max(u, axis=0))
-
-                scaling = (len(self.solver.mesh.faces)) / C
-
-                # update rho
-                # dC_drho = p * E_0 * rho**(p-1) * C_faces
-                sensitivity = 2*(C - target_compliance) * p * E_0 * rho**(p-1) * C_faces # gradient of objective vs rho
-                rho = self.oc_density(rho, sensitivity, volume_frac)
-
-                # print('avg. rho change:', self.solver.mesh.calculate_mean_value(alpha * scaling * dobjective_drho))
-                # print('avg. vol correction:', beta * min(volume_frac - rho_mean, 0))
-
-                # smoothing
-                smoothed_rho = np.zeros(len(rho))
-                for face_idx, face in enumerate(self.solver.mesh.faces):
-                    neighbor_value = np.mean([rho[neighbor_idx] for neighbor_idx in face_neighbors[face_idx]])
-                    smoothed_rho[face_idx] = 0.5 * neighbor_value + 0.5 * rho[face_idx]
-                rho = np.clip(smoothed_rho, 0, 1)
-
-                us.append(u.copy())
-                deformed_meshes.append(deformed_mesh)
-                rhos.append(rho.copy())
-                compliances.append(C)
-
-            self.solution.set_values('us', us)
-            self.solution.set_values('deformed_meshes', deformed_meshes)
-            self.solution.set_values('rhos', rhos)
-            self.solution.set_values('compliances', compliances)
-        else:
-            raise ValueError(f'Topology optimization not supported for {self.equation.name} equation')
-        pass
-
-    def solve_with_target_compliance(self, target_compliance):
-        he_mesh = HalfEdgeMesh(self.solver.mesh)
-        if self.solver.equation.name == 'linear_elastic':
-            E_0 = self.solver.equation.parameters['E']
-            iters, volume_frac = self.parameters['iters'], self.parameters['volume_frac']
-            alpha, beta = self.parameters['alpha'], self.parameters['beta']
-            
-            E_min, p = 1e-9, 3
-            rho = np.full(len(self.solver.mesh.faces), volume_frac)
-            
-            rhos, us, deformed_meshes, compliances = [], [], [], []
-            for iter in range(iters+1):
-                self.solver.equation.parameters['E'] = E_min + E_0 * rho**p
-                self.solver.solve()
-                C = self.solver.solution.values['total_compliance']
-                C_faces = self.solver.solution.values['compliance']
-                rho_mean = self.solver.mesh.calculate_mean_value(rho)
-
-                u, deformed_mesh = self.solver.solution.values['u'], self.solver.solution.values['deformed_mesh']
-
-                # print results
-                print('\nIteration', iter)
-                print('C:', C)
-                print(f'Volume ratio: {rho_mean}')
-                print('max displacement', np.max(u, axis=0))
-
-                if iter == 0:
-                    scaling = (len(self.solver.mesh.faces)) / C
-                elif iter == iters:
-                    # for last iter, want to keep rho and soln consistent
-                    break
-
-                # update rho
-                # dC_drho = p * E_0 * rho**(p-1) * C_faces
-                sensitivity = 2*(C - target_compliance) * p * E_0 * rho**(p-1) * C_faces # gradient of objective vs rho
-                rho = self.oc_density(rho, sensitivity, volume_frac)
-
-                # print('avg. rho change:', self.solver.mesh.calculate_mean_value(alpha * scaling * dobjective_drho))
-                # print('avg. vol correction:', beta * min(volume_frac - rho_mean, 0))
-
-                # smoothing
-                smoothed_rho = np.zeros(len(rho))
-                for face_idx, face in enumerate(self.solver.mesh.faces):
-                    neighbor_value = np.mean([rho[f_idx] for f_idx in he_mesh.get_f_neighbor_f_idxs(face_idx)])
-                    smoothed_rho[face_idx] = 0.4 * neighbor_value + 0.6 * rho[face_idx]
-                rho = smoothed_rho
-
-                rho = np.clip(rho, 0, 1)
-
-                us.append(u.copy())
-                deformed_meshes.append(deformed_mesh)
-                rhos.append(rho.copy())
-                compliances.append(C)
-
-            self.solution.set_values('us', us)
-            self.solution.set_values('deformed_meshes', deformed_meshes)
-            self.solution.set_values('rhos', rhos)
-            self.solution.set_values('compliances', compliances)
-        else:
-            raise ValueError(f'Topology optimization not supported for {self.equation.name} equation')
-        pass
-    

main.py
@@ -50,7 +50,8 @@ if __name__ == '__main__':
 
     # MESH
     # MESH_FILE = 'meshes/hole1000_mesh.pkl'
-    MESH_FILE = 'meshes/square1000_mesh.pkl'
+    # MESH_FILE = 'meshes/square1000_mesh.pkl'
+    MESH_FILE = 'meshes/spring_long.pkl'
     # MESH_FILE = 'meshes/regular_mesh3.pkl'
     # MESH_FILE = 'meshes/easy_rectangle.pkl'
     mesh = Mesh.load(MESH_FILE)
@@ -128,8 +129,6 @@ if __name__ == '__main__':
     # axs = [fig.add_subplot(121, projection='3d'), fig.add_subplot(122)]
     # new_solver.solution.plot_surface('u', title='Poisson Solution', ax=axs[0], show=False)
     # new_solver.solution.plot_colored('face_residuals', title='Poisson Residuals', ax=axs[1])
-
-
     
     # # ADAPTIVE REFINEMENT TEST
     # initial_mesh = new_solver.mesh
@@ -186,29 +185,31 @@ if __name__ == '__main__':
     # # solver.solution.plot_animation('u_values')
 
     # LINEAR ELASTICS
-    def force(point):
-        if point[1] < 1e-6:
-            return np.array([[0, -1]])
-        return np.array([[0, 0]])
+    def down_force(point):
+        if point[0] > w - 1e-6 and np.abs(point[1] - h/2) < 0.1:
+            return np.array([0, -50])
+        return np.array([0, 0])
 
     beam_bc = BoundaryConditions(mesh)
     left_idxs = [idx for idx in boundary_idxs if points[idx][0] < 1e-6]
     right_idxs = [idx for idx in boundary_idxs if points[idx][0] > w-1e-6]
     beam_bc.add('dirichlet', left_idxs, [[0, 0] for idx in left_idxs])
-    beam_bc.add('dirichlet', right_idxs, [[0, 0] for idx in right_idxs])
+    # beam_bc.add('neumann', right_idxs, [[10, 0] for idx in right_idxs])
 
     equation = Equation('linear_elastic', {'E': 125, 'nu': 0.4})
-    solver = Solver(mesh, equation, beam_bc, load_function=force)
+    solver = Solver(mesh, equation, beam_bc, load_function=down_force)
     solver.solve()
     # solver.solution.plot_deformed('stress')
     solver.solution.plot_colored('stress', save="results/stress.png")
 
     # TOPOLOGY OPTIMIZATION
-    topoptimizer = TopologyOptimizer(solver, {'iters': 25, 'volume_frac': 0.5, 'alpha': 0.002, 'beta': 0.6})
+    topoptimizer = TopologyOptimizer(solver, {'iters': 25, 'volume_frac': 0.5})
     topoptimizer.solve()
     # topoptimizer.solution.plot_animation('rhos', save='results/topopt.png')
     topoptimizer.solution.plot_colored('rhos', idx=10, save="results/rho10.png")
     topoptimizer.solution.plot_colored('rhos', idx=20, save="results/rho20.png")
+    topoptimizer.solution.plot_animation('rhos', save='results/topopt.gif', cbar_lim=[0, 1])
+
 
     # save_topopt_results(topopt_results, name='topopt_1', fps=30)
 

spring_design.py
@@ -10,82 +10,57 @@ from TopologyOptimizer import *
 from utils.helper import *
 from Mesh import *
 
-# # Generate rectangular mesh
-# corners = [[0, 0], [2, 1]]
-# mesher = RectMesher(corners, resolution=(80, 40))
-# mesh = mesher.mesh()
-# mesh.save('meshes/spring_80x40.pkl')
-# mesh.plot(save='temp.png')
- 
+def zero_force(point):
+    return np.array([[0, 0]])
 
-# Load Mesh
+# possible issue with compliance calculation
+    # weighted by rho?
+    # plot against mesh next to rho
+# mesh overlap issue
+
+# Mesh
 MESH_FILE = 'meshes/spring_80x40.pkl'
 mesh = Mesh.load(MESH_FILE)
 points, faces, boundary = mesh.get_info()
-boundary_idxs = list(set(boundary.ravel()))
-mesh.plot(save="temp.png")
-
 w, h = np.max(mesh.points[:, 0]), np.max(mesh.points[:, 1])
-
-
-# Boundary conditions
-def zero_force(point):
-    return np.array([[0, 0]])
-def right_force(point):
-    if point[0] > w-1e-6:
-        return np.array([[0, -100]])
-    return np.array([[0, 0]])
-
-spring_bc = BoundaryConditions(mesh)
+boundary_idxs = list(set(boundary.ravel()))
 left_idxs = [idx for idx in boundary_idxs if points[idx][0] < 1e-6]
 right_idxs = [idx for idx in boundary_idxs if points[idx][0] > w-1e-6]
-spring_bc.add('dirichlet', left_idxs, [[0, 0] for idx in left_idxs])
-spring_bc.add('dirichlet', right_idxs, [[0.5, 0] for idx in right_idxs])
+mesh.plot(save="temp.png")
 
-# Solve
-equation = Equation('linear_elastic', {'E': 125, 'nu': 0.4})
-solver = Solver(mesh, equation, spring_bc, load_function=zero_force)
-solver.solve()
-solver.solution.plot_deformed('compliance', save="results/stress_deformed.png")
-print("compliance:", solver.solution.get_values("total_compliance"))
+# Density
+with open('rho_bistable1.pkl', 'rb') as f:
+    rho = pickle.load(f)
 
+mesh.plot_colored(rho, save="temp2.png")
 
-# # TOPOLOGY OPTIMIZATION
-# topoptimizer = TopologyOptimizer(solver, {'iters': 25, 'volume_frac': 0.5, 'alpha': 0.0002, 'beta': 0.0})
-# topoptimizer.solve()
-# # topoptimizer.solve_with_target_compliance(1.0)
-# topoptimizer.solution.plot_colored('rhos', idx=10, save="results/rho10.png")
-# topoptimizer.solution.plot_colored('rhos', idx=20, save="results/rho20.png")
-# topoptimizer.solution.plot_animation('rhos', save='results/topopt.gif', cbar_lim=[0, 1])
+def solve_with_rho(rho, bc):
+    equation = Equation('linear_elastic', {'E': 125, 'nu': 0.4, 'rho': np.array(rho)})
+    solver = Solver(mesh, equation, bc, load_function=zero_force)
+    solver.solve()
+    return solver.solution
 
+def test_compliance_vs_displacements(rho, displacements):
+    results = []
+    for dx in displacements:
+        print(dx)
+        bc = BoundaryConditions(mesh)
+        bc.add('dirichlet', left_idxs, [[0, 0] for idx in left_idxs])
+        bc.add('dirichlet', right_idxs, [[dx, 0] for idx in right_idxs])
+        solution = solve_with_rho(rho, bc)
+        results.append([dx, solution.__copy__()])
 
-# # Plot compliance vs displacement
-# results = []
+        # mid way plotting
+        compliance = solution.get_values('total_compliance')
+        fig, ax = plt.subplots(3, 1)
+        solution.plot_deformed('rho', ax=ax[0], title=f'Disp.={dx}, E={compliance:.4f}')
+        solution.plot_deformed('force_faces', ax=ax[2], title=f'Disp.={dx}, E={compliance:.4f}')
+        solution.plot_deformed(None, ax=ax[1], save=f"temp3.png", title=f'Disp.={dx}, E={compliance:.4f}')
 
-# displacements = np.linspace(-0.5, 0.5, 5, endpoint=True)
-# for dx in displacements:
-#     bc = BoundaryConditions(mesh)
-#     bc.add('dirichlet', left_idxs, [[0, 0] for idx in left_idxs])
-#     bc.add('dirichlet', right_idxs, [[dx, 0] for idx in right_idxs])
-#     solver = Solver(mesh, equation, bc, load_function=zero_force)
-#     solver.solve()
-    
-#     compliance = solver.solution.get_values("total_compliance")
-#     results.append([dx, compliance])
+    return results
 
+displacements = np.linspace(-0.4, 1.0, 9, endpoint=True)
+results = test_compliance_vs_displacements(rho, displacements)
 
-# # plot results
-# results = np.array(results)
-# plt.plot(results[:, 0], results[:, 1])
-# plt.xlabel("Displacement")
-# plt.ylabel("Compliance")
-# plt.title("Compliance vs Displacement")
-# plt.savefig("results/compliance_vs_displacement.png")
-# plt.close()
-
-
-# print("\n done")
-
-
-# # # debugging
-# # mesh.plot(save="temp.png", idxs=left_idxs)
+with open('results.pkl', 'wb') as f:
+    pickle.dump(results, f)